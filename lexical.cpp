e0619fcce8028a8a6523e293c50fdb12990621bf
beinyoyo <sanjaymakwana02@mail.com>
2019-03-12 12:32:13 +0530

    1st

---
+++ lexical.cpp
@@ -0,0 +1,10 @@
+#include<list>
+#include<iostream>
+#include<fstream>
+#include<string>
+
+//Structures for the various tokens
+struct arith_operators{string ar_op;};
+struct keywords{string key;};
+struct rel_operators{string re_op;};
+struct bitw_operators{string bit_op;};
