67c1437489b7ea357c769ea9e1eef52a153f3df8
beinyoyo <sanjaymakwana02@mail.com>
2019-03-12 13:09:30 +0530

    update

--- lexical.cpp
+++ lexical.cpp
@@ -5,7 +5,7 @@
 #include<map>
 #include<regex>
 
-//Structures for the various tokens
+//Structures for tokens
 struct token_format_entry
 {
 	regex format;
@@ -14,18 +14,48 @@ struct token_format_entry
 
 struct token_entry
 {
-		
+	string token;
+	string type;
 }
+void tokenize(string word, vector<token_format_entry> &TokenFormat, vector<token_entry> &tokens)
+{
+	char c_word = word.c_str();
+	int i;
+	int start=0, end, len = word.size();
+
+	string token="", next_token="";
+
+//Traverse word char by char
+	for(i=0;i<len;i++)
+	{
+		next_token.append(c_word[i]);
+		if(is_valid(next_token, TokenFormat))
+		{
+			token=next_token;
+		}
+		else
+		{
+			tokens.push()
+		}
+
+	}
+}
+
 
 int main()
 {
 	// file 
 	ifstream file("prog.txt");
-	string line;
+	string word;
 
 	//Database
 	vector<token_format_entry> TokenFormat;
 	vector<token_entry> tokens;
 
 	//Read file
+	while( file >> word )
+	{
+		tokenize(word, TokenFormat, tokens);
+	}
+	return 0;
 }
